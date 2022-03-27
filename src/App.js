3bca9778dfd4888bc2363ec1401f30e419ed60ab
deestaton <82401671+deestaton@users.noreply.github.com>
2022-03-26 22:44:11 -0700

    Begin working on navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <Navbar />
       </header>
     </div>
   );
